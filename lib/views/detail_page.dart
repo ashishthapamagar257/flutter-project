f13c819373b1dfc75ff1a88233a676df049b5e9f
ashishthapamagar257 <magaraashish54321@gmail.com>
2023-12-17 22:02:29 +0545

    change

--- lib/views/detail_page.dart
+++ lib/views/detail_page.dart
@@ -1,26 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-//
-// class DetailPage extends ConsumerWidget {
-//   final Movie movie;
-//   const DetailPage({super.key, required this.movie});
-//
-//   @override
-//   Widget build(BuildContext context, ref) {
-//     final state = ref.watch(VideoProvider(movie.id));
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Column(
-//           children: [
-//             state.when(
-//
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+
 
 
 import 'package:firstapp/provider/food_povider.dart';
