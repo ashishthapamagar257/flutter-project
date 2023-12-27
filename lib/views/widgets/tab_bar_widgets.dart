21024f29fc545bb177f522d757b2a1c651067e75
ashishthapamagar257 <magaraashish54321@gmail.com>
2023-12-27 17:14:10 +0545

    changes

--- lib/views/widgets/tab_bar_widgets.dart
+++ lib/views/widgets/tab_bar_widgets.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_offline/flutter_offline.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
-
+import 'package:lottie/lottie.dart';
 
 
 class TabBarWidgets extends ConsumerWidget {
@@ -15,57 +15,70 @@ class TabBarWidgets extends ConsumerWidget {
   Widget build(BuildContext context, ref) {
     final state = ref.watch(movieProvider(api));
     return OfflineBuilder(
-      child: Container(),
-      connectivityBuilder: (
-          BuildContext context,
-      ConnectivityResult connectivity,
-      Widget child) {
-        final bool connected = connectivity != ConnectivityResult.none;
-
-        return connected ? state.when(
-            data: (data) {
-              return Padding(
-                padding: EdgeInsets.all(8.0),
-                child: GridView.builder(
-                    key: PageStorageKey(api),
-                    itemCount: data.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 5,
-                        mainAxisSpacing: 5,
-                        childAspectRatio: 2 / 3
-                    ),
-                    itemBuilder: (context, index) {
-                      final movie = data[index];
-                      return InkWell(
-                          onTap: () {
-                            Get.to(() => DetailPage(movie: movie));
+        child: Container(),
+        connectivityBuilder: (
+            BuildContext context,
+            ConnectivityResult connectivity,
+            Widget child) {
+          final bool connected = connectivity != ConnectivityResult.none;
+          return  state.when(
+              data: (data) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: GridView.builder(
+                      key: PageStorageKey(api),
+                      itemCount: data.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 5,
+                          mainAxisSpacing: 5,
+                          childAspectRatio: 2 / 3
+                      ),
+                      itemBuilder: (context, index) {
+                        final movie = data[index];
+                        return InkWell(
+                            onTap: () {
+                              Get.to(() => DetailPage(movie: movie));
+                            },
+                            child: Image.network(movie.poster_path));
+                      }
+                  ),
+                );
+              },
+              error: (err, st) {
+                if(connected){
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Lottie.asset('assets/images/internet.json'),
+                      ElevatedButton(
+                          onPressed: (){
+                            ref.invalidate(movieProvider);
                           },
-                          child: Image.network(movie.poster_path));
-                    }
-                ),
-              );
-            },
-            error: (err, st) {
-             if (connected){
-              return Center(child: Text('$err'));
-             }else{
-               return Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: [
-                   const Center(child: Text('No - Connection')),
-                   ElevatedButton(
-                       onPressed: (){
+                          child: Text('click to refresh')
+                      )
+                    ],
+                  );
+                }else{
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Lottie.asset('assets/images/noConnect.json'),
+                      ElevatedButton(
+                          onPressed: (){
+                            ref.invalidate(movieProvider(api));
+                          },
+                          child: Text('click to refresh')
+                      )
+                    ],
+                  );
+                }
 
-                   }, child: const Text('Click to Refresh')
-                   )
-                 ],
-               );
-             }
-            },
-            loading: () =>  Center(child: CircularProgressIndicator())
-        );
-      }
+              },
+              loading: () => Center(child: CircularProgressIndicator())
+          );
+        }
     );
   }
 }
+
